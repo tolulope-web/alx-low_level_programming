1fee7c4fb48a83474d6ee42b4b714d1a4c976ca3
tolulope <tolulope24049@gmail.com>
2022-03-10 18:02:05 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints output
+ *
+ * Return 0 if true
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2005-10-19\n , 59);
+	return (1);
+}
