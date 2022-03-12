61136d8febf266b29f41869b35a880a8e5c59585
tolulope <tolulope24049@gmail.com>
2022-03-12 15:33:06 -0800

    comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Description: print all posible cominations of two numbers
+ * numbers shold range from 0-99
+ * 3 digits must be different
+ * You can only use putchar up to 6 times
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i < 1000; i++)
+	{
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
+		if (j < k && k < 1)
+		{
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+			if (i < 700)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			}
+	}
+	putchar('\n');
+	return (0);
+}
