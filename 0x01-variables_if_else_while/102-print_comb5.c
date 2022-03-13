ac31f892c871213f65ece4fe241e295eda5b794b
tolulope <tolulope24049@gmail.com>
2022-03-13 10:43:01 -0700

    comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ * main - block
+ * Description: the numbers should range from 0 to 99
+ * the numbers should be separated by space
+ * maximum of 8 'putchar' in the code
+ * all code must be in the main function
+ */
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+		for (i = 0; i < 100; i++)
+		{
+			c = i / 10;
+			d = i % 10;
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
