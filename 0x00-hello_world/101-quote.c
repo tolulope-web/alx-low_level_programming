85181c987f20cc262ca23e807716e5337632cb30
tolulope <tolulope24049@gmail.com>
2022-03-14 04:13:45 -0700

    git commit -m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints output
- *
- * Return 0 if true
+ * main - prints exactly "and that place of art is useful"
+ *  - Dora Korpar, 2015-10-19",
+ *  followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2005-10-19\n" , 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
