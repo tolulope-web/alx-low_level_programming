c060905f256a911a09018cb9442ca2fd4046424f
tolulope <tolulope24049@gmail.com>
2022-03-14 14:38:11 -0700

    print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,25 +9,29 @@
  */
 int main(void)
 {
-	int i, j, k, l;
+	int n, m, l;
 
-	for (i = 0; i < 1000; i++)
+	for (n = 48; n < 58; n++)
 	{
-		j = i / 100;
-		k = (i / 10) % 10;
-		l = i % 10;
-		if (j < k && k < 1)
+		for (m = 49; m < 58; m++)
 		{
-			putchar(j + '0');
-			putchar(k + '0');
-			putchar(l + '0');
-			if (i < 700)
+			for (l = 50; l < 58; l++)
 			{
-				putchar(44);
-				putchar(32);
+				if (l > m && m > n)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
 			}
-			}
+		}
 	}
 	putchar('\n');
 	return (0);
 }
+
