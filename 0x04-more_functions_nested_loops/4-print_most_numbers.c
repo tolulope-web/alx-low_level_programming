e0b29b5f8c7954cc50543626e46e10943722061e
tolu <tolulope24049@gmail.com>
2022-03-18 21:26:43 +0100

    add

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print 0 to 9 jump 2 and 4
- *
- *
- */
+* print_most_numbers - print 0 to 9 jump 2 and 4
+*
+*
+*/
 void print_most_numbers(void)
 {
-    char i;
+	char i;
 
-    for (i = '0'; i <= '9'; i++)
-    {
-        if (i != '2' && i != '4')
-            _putchar(i);
-    }
-    _putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+	{
+	if (i != '2' && i != '4')
+	_putchar(i);
+	}
+	_putchar('\n');
+
 }
